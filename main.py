68878a4d397b59b581eb810d8974aeb219fdc952
thiennguyen <nl.chuongthien@gmail.com>
2019-12-31 15:55:05 +0800

    typo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from common_function import *
 def main():
     print("This is Main")
     for i in range(5):
-        photos_list = read_file(3)
+        photos_list = read_file(i)
         order_slide = start_solution_1(photos_list)
         final_scoring_system(order_slide)
         print("-----------------------------------")
