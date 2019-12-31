bab9ae1a554cf8fe2d49f4dcc33bc6e74d448fca
thiennguyen <nl.chuongthien@gmail.com>
2019-12-31 15:58:42 +0800

    Extract individual score

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from common_function import *
 
 def main():
     print("This is Main")
+    final_score = []
     for i in range(5):
         photos_list = read_file(i)
         order_slide = start_solution_1(photos_list)
-        final_scoring_system(order_slide)
+        final_score.append(final_scoring_system(order_slide))
         print("-----------------------------------")
         print()
+
+    print("Final Individual Score")
+    print(final_score)
 
 
 if __name__ == "__main__":
